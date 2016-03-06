0f71adea3a2dc7eace87d8cc57a44a450e6a972a
@sleepyfox <sleepyfox@gmail.com>
2016-03-06 10:53:34 +0000

    [feature] compute diagonals

--- chapter02/src/Main.purs
+++ chapter02/src/Main.purs
@@ -1,9 +1,14 @@
 module Main where
 
-import Prelude (Unit)
+import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, print)
+import Math (sqrt)
+
+diagonal :: Number -> Number -> Number
+diagonal width height = sqrt (width * width + height * height)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  print "The diagonal of a triangle of sides 3 and 4 is:"
+  print (diagonal 3.0 4.0)
